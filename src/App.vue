50524aefbc50df1a13def3a34cbd9dc9c9fc9042
ysr <1365112278@qq.com>
2023-08-26 22:38:49 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <i class="iconfont icon-jilu"></i>
+    <i class="iconfont icon-jilu"></i>
+    <i class="iconfont icon-jilu"></i>
+  </div>
+</template>
+
+<style scoped>
+@import url(./assets/css/index.scss);
+</style>
---
+++ src/components/Tabbar/tabbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>
+      <i class=""></i>
+    </div>
+  </div>
+</template>
+<script setup>
+
+</script>
